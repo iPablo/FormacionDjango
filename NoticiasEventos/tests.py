a7a6af278f7be6c7cf5a3ec2ee8039046c436bf6
jchierro <jchierro@hotmail.com>
2016-03-29 14:20:23 +0200

    Mejorando Test's

--- NoticiasEventos/tests.py
+++ NoticiasEventos/tests.py
@@ -4,8 +4,19 @@ from django.utils import timezone
 from django.core.urlresolvers import reverse
 from django.test.client import Client
 from .forms import NewsItemForm, EventForm
+import ipdb
+
+### NOTA: ORGANIZANDO 
 
 class NewsItemTestCase(TestCase):
+
+	def addNewsItem(self):
+		form = NewsItemForm(data={'title': 'test title', 
+			'description': 'test description', 
+			'publish_date': timezone.now()})
+		form.is_valid()
+		form.save()
+		return NewsItem.objects.get(title="test title")
 
 	def test_index_v2List(self):
 		'''Comprueba el acceso a v2List'''
@@ -14,15 +25,28 @@ class NewsItemTestCase(TestCase):
 		self.assertContains(response, "No items found.")
 		self.assertQuerysetEqual(response.context['result'], [])
 
-	def test_form_isTrue_v2Create_v2Update(self):
-		'''Comprobar si el formulario es correcto'''
-		form = NewsItemForm(data={'title': 'prueba title', 'description': 'prueba description', 'publish_date': timezone.now()})
+	def test_form_create_newsItem(self):
+		'''Comprueba la validacion del formulario y la creacion del objeto NewsItem'''
+		form = NewsItemForm(data={'title': 'prueba title', 
+			'description': 'prueba description', 
+			'publish_date': timezone.now()})
 		self.assertTrue(form.is_valid())
+		form.save()
+		x = NewsItem.objects.get(title="prueba title")
+		self.assertTrue(x)
 
-	def test_form_isFalse_v2Create_v2Update(self):
-		'''Comprobar si el formulario no es correcto'''
-		form = NewsItemForm(data={'title': '', 'description': '', 'publish_date': timezone.now()})
-		self.assertFalse(form.is_valid())
+	def test_form_update_newsItem(self):
+		'''Comprueba el acceso a v1Update'''
+		x = self.addNewsItem()
+		response = self.client.get(reverse('NoticiasEventos:v1Update', kwargs = {'pk': x.id}))
+		self.assertEqual(response.status_code, 200)
+		
+
+	def test_delete_v1(self):
+		'''Comprueba si se borra correctamente'''
+		x = self.addNewsItem()
+		self.client.get(reverse('NoticiasEventos:v1Delete', kwargs = {'pk':x.id}))
+		self.assertQuerysetEqual(NewsItem.objects.all(), [])
 
 	def test_index_v1List(self):
 		'''Comprueba el acceso a v1List'''
@@ -31,17 +55,5 @@ class NewsItemTestCase(TestCase):
 		self.assertContains(response, "No items found.")
 		self.assertQuerysetEqual(response.context['result'], [])
 
-	def test_delete(self):
-		'''Comprueba el acceso a index'''
-		#response = self.client.get(reverse('NoticiasEventos:v2Delete'))
-		#self.assertEqual(response.status_code, 200)
-
-
-
-
-
-
-		
 	
-
-
+		
